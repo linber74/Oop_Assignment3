577e86db637f760719cd68807dc15d455bb2dd52
Linda Bergin <linber24@gmail.com>
2025-10-31 18:59:36 +0100

    init commit, started GameModel

---
+++ src/GameModel.java
@@ -0,0 +1,11 @@
+public class GameModel {
+    private final int rows =4;
+    private final int cols =4;
+    private int [][]board = new int[rows][cols];
+    private int emptyRow;
+    private int emptyCol;
+
+    public GameModel() {
+
+    }
+}
