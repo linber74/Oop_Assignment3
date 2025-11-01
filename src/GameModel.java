9a0cd496184c73bc741361ea119acd47b5c49d3c
Linda Bergin <linber24@gmail.com>
2025-11-01 21:21:30 +0100

    Created constuctor and method initBoardSolved

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,11 +1,21 @@
 public class GameModel {
-    private final int rows =4;
-    private final int cols =4;
-    private int [][]board = new int[rows][cols];
+    private int[][] board = new int[4][4];
     private int emptyRow;
     private int emptyCol;
 
     public GameModel() {
+        initBoardSolved();
 
+    }
+
+    public void initBoardSolved() {
+        board  = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 0}
+        };
+        emptyRow = 3;
+        emptyCol = 3;
     }
 }
