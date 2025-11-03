fd2030046fcfbccb2c6e95f07213de1f59bcd4de
Linda Bergin <linber24@gmail.com>
2025-11-03 16:34:49 +0100

    Created shuffle

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GameModel {
     private int[][] board = new int[4][4];
     private int emptyRow;
@@ -17,5 +21,34 @@ public class GameModel {
         };
         emptyRow = 3;
         emptyCol = 3;
+    }
+
+    private static final int SIZE =4;
+    private static final int TILE_COUNT = 16;
+
+    public void shuffle(){
+        List<Integer> tiles = new ArrayList<>();
+        for(int i = 0; i < TILE_COUNT; i++){
+            tiles.add(1);
+        }
+        do {
+            Collections.shuffle(tiles);
+        }while (!isSolvable ());
+
+        int index =0;
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                int value = tiles.get(index++);
+                board[row][col] = value;
+                if (value == 0){
+                    emptyRow = row;
+                    emptyCol = col;
+                }
+            }
+        }
+    }
+
+    public boolean isSolvable(){
+
     }
 }
