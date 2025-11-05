473bf2a2312457616dd2e30047d249b494074c75
Linda Bergin <linber24@gmail.com>
2025-11-05 03:48:04 +0100

    updated gameview

--- src/GameVeiw.java
+++ src/GameVeiw.java
@@ -4,29 +4,60 @@ import java.awt.*;
 public class GameVeiw extends JFrame {
 
     GameModel model;
+    JPanel bottomPanel;
+    JPanel topPanel;
     JPanel boardPanel;
     JLabel titleLabel;
     JLabel statusLabel;
-    JButton[][] buttons = new JButton[4][4];
+    JButton startButton;
+    JButton[][] buttons;
 
     public GameVeiw() {
         model = new GameModel();
 
-        boardPanel = new JPanel();
-        titleLabel = new JLabel("15");
-        statusLabel = new JLabel();
+        int row = 4;
+        int col = 4;
 
+
+        topPanel = new JPanel();
+        bottomPanel = new JPanel();
+        boardPanel = new JPanel();
+        statusLabel = new JLabel();
+        buttons = new JButton[row][col];
+        startButton = new JButton("Nytt Spel");
+
+        topPanel.setLayout(new FlowLayout());
+        topPanel.add(titleLabel);
+        topPanel.add(startButton);
+
+        boardPanel.setLayout(new GridLayout(4, 4));
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                buttons[i][j] = new JButton();
+                buttons[i][j] = new JButton("[" + i + "," + j + "]");
+                boardPanel.add(buttons[i][j]);
             }
         }
 
-        boardPanel.setLayout(new GridLayout(4, 4));
+
+        bottomPanel.setLayout(new FlowLayout());
+        bottomPanel.add(statusLabel);
+
+        setLayout( new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        add(boardPanel, BorderLayout.CENTER);
+        add(topPanel, BorderLayout.NORTH);
+
+        setTitle("15");
+        pack();
+        setVisible(true);
+        setLocation(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
         updateView();
     }
 
-    public void updateView() {}
+    public void updateView() {
+    }
 
 }
