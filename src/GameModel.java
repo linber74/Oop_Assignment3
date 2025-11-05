82e3db6dce64de0bf2e961b785f27d935b8124da
Linda Bergin <linber24@gmail.com>
2025-11-05 04:57:17 +0100

    updated gameview method updateView and create getValue in gameModel

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,11 +1,109 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GameModel {
-    private final int rows =4;
-    private final int cols =4;
-    private int [][]board = new int[rows][cols];
+    private int[][] board = new int[4][4];
     private int emptyRow;
     private int emptyCol;
 
     public GameModel() {
+        initBoardSolved();
 
+    }
+
+    public void initBoardSolved() {
+        board  = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 0}
+        };
+        emptyRow = 3;
+        emptyCol = 3;
+    }
+
+    private static final int SIZE =4;
+    private static final int TILE_COUNT = 16;
+
+    public void shuffle(){
+        List<Integer> tiles = new ArrayList<>();
+        for(int i = 0; i < TILE_COUNT; i++){
+            tiles.add(i);
+        }
+        do {
+            Collections.shuffle(tiles);
+        }while (!isSolvable (tiles));
+
+        int index =0;
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                int value = tiles.get(index++);
+                board[row][col] = value;
+                if (value == 0){
+                    emptyRow = row;
+                    emptyCol = col;
+                }
+            }
+        }
+    }
+
+    // Lösbarhetsalgoritm baserad på inverser och tom ruta
+    // Källa: https://www.geeksforgeeks.org/dsa/check-instance-15-puzzle-solvable/
+
+    public boolean isSolvable(List<Integer> tiles) {
+        int inversions = 0;
+        for (int i = 0; i < tiles.size(); i++) {
+            for (int j = i + 1; j < tiles.size(); j++) {
+                int a = tiles.get(i);
+                int b = tiles.get(j);
+                if (a != 0 && b != 0 && a > b) inversions++;
+            }
+        }
+
+        int zeroIndex = tiles.indexOf(0);
+        int rowFromBottom = 3 - (zeroIndex / 4);
+        return (rowFromBottom % 2 == 0) == (inversions % 2 == 0);
+    }
+
+    public boolean canMove(int row, int col) {
+        if(row < 0 || row >= SIZE || col < 0 || col >= SIZE) return false;
+        int rowdifference = row - emptyRow;
+        int coldifference = col - emptyCol;
+        return (rowdifference + coldifference == 1);
+    }
+
+    public boolean move(int row, int col) {
+        if (canMove(row, col)) {
+            int temp = board[row][col];
+            board[row][col] = board[emptyRow][emptyCol];
+            board[emptyRow][emptyCol] = temp;
+
+            emptyRow = row;
+            emptyCol = col;
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSolved() {
+        int expected = 1;
+        for(int row = 0; row < SIZE; row++){
+            for(int col = 0; col < SIZE; col++){
+                if(row == SIZE - 1 && col == SIZE - 1){
+                    if (board[row][col] != 0) return false;
+                }
+                else {
+                    if (board[row][col] != expected) return false;
+                    expected++;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int getValue(int row, int col) {
+        return board[row][col];
     }
 }
--- src/GameVeiw.java
+++ src/GameVeiw.java
@@ -22,7 +22,6 @@ public class GameVeiw extends JFrame {
         topPanel = new JPanel();
         bottomPanel = new JPanel();
         boardPanel = new JPanel();
-        titleLabel = new JLabel("15");
         statusLabel = new JLabel();
         buttons = new JButton[row][col];
         startButton = new JButton("Nytt Spel");
@@ -43,12 +42,44 @@ public class GameVeiw extends JFrame {
         bottomPanel.setLayout(new FlowLayout());
         bottomPanel.add(statusLabel);
 
+        setLayout( new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        add(boardPanel, BorderLayout.CENTER);
+        add(topPanel, BorderLayout.NORTH);
+
+        setTitle("15");
+        pack();
+        setVisible(true);
+        setLocation(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         updateView();
     }
 
     public void updateView() {
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[i].length; j++) {
+                int value = model.getValue (i, j);
+
+                if(value == 0){
+                    buttons[i][j].setText("");
+                    buttons[i][j].setBackground(Color.blue);
+                    buttons[i][j].setEnabled(false);
+                }
+                else{
+                    buttons[i][j].setText(String.valueOf(value));
+                    buttons[i][j].setBackground(null);
+                    buttons[i][j].setEnabled(true);
+                }
+            }
+        }
+        if (model.isSolved()){
+            statusLabel.setText("DU VANN!!!");
+        }
+        else{
+            statusLabel.setText("");
+        }
     }
 
 }
