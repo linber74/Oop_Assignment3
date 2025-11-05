278fa19e44b82d5a864838b768dcb521e1b00109
Linda Bergin <linber24@gmail.com>
2025-11-05 03:27:49 +0100

    Create updateView

--- src/GameVeiw.java
+++ src/GameVeiw.java
@@ -4,29 +4,51 @@ import java.awt.*;
 public class GameVeiw extends JFrame {
 
     GameModel model;
+    JPanel bottomPanel;
+    JPanel topPanel;
     JPanel boardPanel;
     JLabel titleLabel;
     JLabel statusLabel;
-    JButton[][] buttons = new JButton[4][4];
+    JButton startButton;
+    JButton[][] buttons;
 
     public GameVeiw() {
         model = new GameModel();
 
+        int row = 4;
+        int col = 4;
+
+
+        topPanel = new JPanel();
+        bottomPanel = new JPanel();
         boardPanel = new JPanel();
         titleLabel = new JLabel("15");
         statusLabel = new JLabel();
+        buttons = new JButton[row][col];
+        startButton = new JButton("Nytt Spel");
 
+        topPanel.setLayout(new FlowLayout());
+        topPanel.add(titleLabel);
+        topPanel.add(startButton);
+
+        boardPanel.setLayout(new GridLayout(4, 4));
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                buttons[i][j] = new JButton();
+                buttons[i][j] = new JButton("[" + i + "," + j + "]");
+                boardPanel.add(buttons[i][j]);
             }
         }
 
-        boardPanel.setLayout(new GridLayout(4, 4));
+
+        bottomPanel.setLayout(new FlowLayout());
+        bottomPanel.add(statusLabel);
+
+
 
         updateView();
     }
 
-    public void updateView() {}
+    public void updateView() {
+    }
 
 }
